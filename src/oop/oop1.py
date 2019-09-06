7e0437f88272afa807d85d8c2179e77c7f39eeeb
azinoVo <azinovo22@gmail.com>
2019-09-06 10:09:57 -0500

    fin oop1

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,33 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+# Base Class
+class Vehicle:
+    pass
+
+# 1st gen children
+class GroundVehicle(Vehicle):
+    pass
+
+class FlightVehicle(Vehicle):
+    pass
+
+# Children of Ground and grandchildren of Vehicle
+class Car(GroundVehicle):
+    pass
+
+class Motorcycle(GroundVehicle):
+    pass
+
+# Children of Flight and grandchildren of Vehicle
+class Airplane(FlightVehicle):
+    pass
+
+class Starship(FlightVehicle):
+    pass
+
+
+
+
+
